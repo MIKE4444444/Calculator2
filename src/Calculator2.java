3b076e779ca247bb487e92562bab1d0352c4c39e
Your name <mike.Roy2711@gmail.com>
2019-01-20 20:56:09 -0800

    ADD Substract method

--- src/Calculator2.java
+++ src/Calculator2.java
@@ -3,5 +3,7 @@ public class Calculator2 {
     public static double add(double num1 , double num2){
         return num1 + num2;
     }
-
+    public static double subtract(double num1 , double num2) {
+        return num1 - num2;
+    }
 }
