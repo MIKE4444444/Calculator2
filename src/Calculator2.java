940a0f6aac4982b11711596ff2e4b3fe8dbd9a0c
MIKE4444444 <42721049+MIKE4444444@users.noreply.github.com>
2019-01-25 12:45:05 -0800

    Updated

--- src/Calculator2.java
+++ src/Calculator2.java
@@ -5,7 +5,7 @@ public class Calculator2 {
     public static double add(double num1 , double num2){
         return num1 + num2;
     }
-    public static double subtract(double num1 , double num2) {
+    public static double subtract(double num1 ,   double num2) {
         return num1 - num2;
     }
        public static double multiply(double num1 , double num2) {
